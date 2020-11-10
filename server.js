2b2f34f36262babb13205a15852bf8987a994e0d
root <root@meething.space>
2020-11-10 22:11:39 +0000

    convert to uWebsockets

--- server.js
+++ server.js
@@ -4,60 +4,61 @@
  * MIT Licensed (C) QXIP 2020
  */
 
-const fs = require("fs");
-const url = require("url");
-const http = require("http");
-const https = require("https");
-const WebSocket = require("ws");
-let debug = process.env.DEBUG || false;
-let config = {};
-config.options = {}
-if (!process.env.hasOwnProperty('SSL')||process.env.SSL == false) {
-  var server = http.createServer();
-  server.listen(process.env.PORT || 3000);
-  if (debug) console.log("HTTP server started",process.env.PORT);
-} else {
-  config.options.key = process.env.SSLKEY ? fs.readFileSync(process.env.SSLKEY) : false;
-  config.options.cert = process.env.SSLCERT ? fs.readFileSync(process.env.SSLCERT) :  false;
-  var server = https.createServer(config.options);
-  server.listen(process.env.PORT || 443);
-  if (debug) console.log("HTTPS server started",process.env.PORT);
-}
+let debug = process.env.DEBUG || true;
 
 // LRU with last used sockets
 const QuickLRU = require("quick-lru");
 const lru = new QuickLRU({ maxSize: 1000, onEviction: false });
 if (debug) console.log("LRU initialized");
 
-server.on("upgrade", async function(request, socket, head) {
-  let parsed = url.parse(request.url,true);
-  if(debug) console.log("parsed",parsed);
-  let pathname = parsed.pathname || "/gun";
-  pathname = pathname.replace(/^\/\//g,'/');
-  if (debug) console.log("Got WS request", pathname);
+const uWS = require('uWebSockets.js');
+const port = process.env.PORT || 443;
+const host = process.env.HOST || '127.0.0.1';
 
-  var gun = { gun: false, server: false };
-  if (pathname) {
-    if (lru.has(pathname)) {
-      // Existing Node
-      if (debug) console.log("Recycle id", pathname);
-      gun = await lru.get(pathname);
-    } else {
-      // Create Node
-      if (debug) console.log("Create id", pathname);
-      // NOTE: Only works with lib/ws.js shim allowing a predefined WS as ws.web parameter in Gun constructor
-      gun.server = new WebSocket.Server({ noServer: true, path: pathname });
-      lru.set(pathname, gun);
-    }
+const app = uWS.SSLApp({
+  key_file_name: process.env.SSLKEY,
+  cert_file_name: process.env.SSLCERT
+}).ws('/*', {
+  /* Options */
+  compression: 0,
+  maxPayloadLength: 16 * 1024 * 1024,
+  idleTimeout: 10,
+
+  /* Handlers */
+  upgrade: (res, req, context) => { // a request was made to open websocket, res req have all the properties for the request, cookies etc
+      // add code here to determine if ws request should be accepted or denied
+      // can deny request with "return res.writeStatus('401').end()" see issue #367
+      res.upgrade( // upgrade to websocket
+         { ip: res.getRemoteAddress(), path: req.getUrl().replace(/^\/|\/$/g, '')||'broadcast' }, // 1st argument sets which properties to pass to the ws object, in this case ip address
+         req.getHeader('sec-websocket-key'),
+         req.getHeader('sec-websocket-protocol'),
+         req.getHeader('sec-websocket-extensions'), // these 3 headers are used to setup the websocket
+         context // also used to setup the websocket
+      )
+  },
+  open: (ws) => {
+    /* Let this client listen to topic "broadcast" */
+    if (debug) console.log('open',ws);
+    ws.subscribe(ws.path);
+  },
+  message: (ws, message, isBinary) => {
+    if (debug) console.log('message',ws,message);
+    /* Broadcast this message */
+    ws.publish(ws.path, message, isBinary);
+  },
+  drain: (ws) => {
+
+  },
+  close: (ws, code, message) => {
+    /* The library guarantees proper unsubscription at close */
+    if (debug) console.log('closed',code,message);
   }
-  if (gun.server) {
-    // Handle Request
-    gun.server.handleUpgrade(request, socket, head, function(ws) {
-      if (debug) console.log("connecting to gun instance", gun.gun.opt()._.opt.ws.path);
-      gun.server.emit("connection", ws, request);
-    });
+}).any('/*', (res, req) => {
+  res.end('Nothing to see here!');
+}).listen(host, port, (token) => {
+  if (token) {
+    console.log('Listening to port ' + port);
   } else {
-    if (debug) console.log("destroying socket", pathname);
-    socket.destroy();
+    console.log('Failed to listen to port ' + port);
   }
 });
